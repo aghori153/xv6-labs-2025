67dcf766ff6a621298e0db54165495f358f7f0ed
Abdul Moid Ghori <abdulmoida13@gmail.com>
2025-09-30 01:17:32 +0500

    Problem Set 1 Submission

--- user/memdump.c
+++ user/memdump.c
@@ -1,3 +1,4 @@
+// user/memdump.c
 #include "kernel/types.h"
 #include "user/user.h"
 
@@ -5,46 +6,101 @@ void
 memdump(char *fmt, char *data)
 {
   for (int i = 0; fmt[i]; i++) {
-    char f = fmt[i];
-    switch (f) {
-    case 'i': {  // 4 bytes: 32-bit int, decimal
-      int val = *(int*)data;
-      printf("%d\n", val);
+    switch (fmt[i]) {
+    case 'i': {
+      int v = *(int*)data;
+      printf("%d\n", v);
       data += 4;
       break;
     }
-    case 'p': {  // 8 bytes: 64-bit integer, hex (lowercase, no 0x)
-      unsigned long long val = *(unsigned long long*)data;
-      printf("%llx\n", val);
+    case 'p': {
+      unsigned long long v = *(unsigned long long*)data;
+      printf("%llx\n", v);
       data += 8;
       break;
     }
-    case 'h': {  // 2 bytes: 16-bit int, decimal
-      short val = *(short*)data;
-      printf("%d\n", val);
+    case 'h': {
+      short v = *(short*)data;
+      printf("%d\n", v);
       data += 2;
       break;
     }
-    case 'c': {  // 1 byte: ASCII character
-      char val = *data;
-      printf("%c\n", val);
+    case 'c': {
+      char v = *data;
+      printf("%c\n", v);
       data += 1;
       break;
     }
-    case 's': {  // pointer to a C string (8-byte pointer)
+    case 's': {
       char *s = *(char**)data;
       printf("%s\n", s);
       data += 8;
       break;
     }
-    case 'S': {  // string starting at current data
+    case 'S': {
       printf("%s\n", data);
       data += strlen(data) + 1;
       break;
     }
     default:
-      // ignore unknown specifiers
       break;
     }
   }
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc == 1) {
+    // ---- Example 1 ----
+    printf("Example 1:\n");
+    int e1[2] = {61810, 2025};
+    memdump("ii", (char*)e1);
+
+    // ---- Example 2 ----
+    printf("Example 2:\n");
+    char ex2[] = "a string";
+    memdump("S", ex2);
+
+    // ---- Example 3 ----
+    printf("Example 3:\n");
+    char ex3[] = "another";
+    memdump("S", ex3);
+
+    // ---- Example 4 ----
+    // Layout: "BD0\0" | int 1819438967 | int 100 | 'z' | (char*) "xyzzy"
+    printf("Example 4:\n");
+    char *tail = "xyzzy";
+    char e4[4 + 4 + 4 + 1 + sizeof(char*)];
+    int off = 0;
+    e4[off++] = 'B';
+    e4[off++] = 'D';
+    e4[off++] = '0';
+    e4[off++] = 0;
+    *(int*)(e4 + off) = 1819438967; off += 4;
+    *(int*)(e4 + off) = 100;        off += 4;
+    e4[off++] = 'z';
+    *(uint64*)(e4 + off) = (uint64)tail;
+    memdump("Siics", e4);  // S -> "BD0", then i, i, c, s
+
+    // ---- Example 5 ----
+    // Layout: "hello\0world"
+    printf("Example 5:\n");
+    char e5[] = "hello\0world";
+    memdump("Sccccc", e5); // "hello", then w o r l d one per line
+
+    exit(0);
+  }
+
+  // Format provided: read stdin into buffer and dump
+  char buf[1024];
+  int n = 0, m;
+  while ((m = read(0, buf + n, sizeof(buf) - n)) > 0) {
+    n += m;
+    if (n >= (int)sizeof(buf)) break;
+  }
+  if (n < (int)sizeof(buf)) buf[n] = 0;
+
+  memdump(argv[1], buf);
+  exit(0);
 }
